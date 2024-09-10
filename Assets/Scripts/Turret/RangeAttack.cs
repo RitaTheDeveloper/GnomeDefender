28f152006983d6c4785f3e7c922d50e3c96b0156
RitaTheDeveloper <ritagrashchenkova@gmail.com>
2024-09-10 20:51:10 +0300

    scripts for unit, idamageable, iattacker

---
+++ Assets/Scripts/Turret/RangeAttack.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeAttack : Ability
+{
+    public override void Execute(IAttacker attacker, IDamageable target)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
